4557d038fb6a194dd83d42694f218df95ec8529e
KariukiAbel <abelmuriithi@gmail.com>
2019-10-12 01:12:51 +0300

    Added a html to ui

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from blockchain import Blockchain
 
@@ -62,13 +62,12 @@ def get_balance():
             'message':'Loading balance failed.',
             'wallet set up':wallet.public_key != None
         }
-        return jsonify(response), 500
-    
+        return jsonify(response), 500  
 
 
 @app.route('/', methods=['GET'])
 def get_ui():
-    return 'This works'
+    return send_from_directory('ui','node.html')
 
 
 @app.route('/transaction', methods=['POST'])
